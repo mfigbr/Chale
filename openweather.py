10a99de9b8ec0870117ebd4aa9a2e27574605f61
mfigbr <marcelo@lynex.com.br>
2019-06-29 18:51:57 -0300

    first commit

---
+++ openweather.py
@@ -0,0 +1,12 @@
+import json
+import requests
+
+api_token = '8af2fc6096188a91c85c8417aaaa8f74'
+api_url = 'api.openweathermap.org/data/2.5/'
+api_param = 'q=Ibiuna,br'
+
+request = 'https://' + api_url + 'weather?' + api_param + '&appid=' + api_token
+print(request)
+
+response = requests.get(request)
+print(response.json())
