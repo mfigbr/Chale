c7cbcb1b39ba895c5ede694b7e5b17d77f8b0727
Marcelo <marcelo@lynex.com.br>
2019-07-21 19:46:38 -0300

    a

--- insert_mongo.py
+++ insert_mongo.py
@@ -1,13 +1,14 @@
-import base64
-import bson
 import pymongo
 import urllib.parse
 
-with open("/home/marcelo/Imagens/20190708/images/T19070816133000.jpg","rb") as imagefile:
-    str = base64.b64encode(imagefile.read())
-bstr = bson.BSON.encode(str)
+#with open("/home/marcelo/Imagens/20190708/images/T19070816133000.jpg","rb") as imagefile:
+#    str = base64.b64encode(imagefile.read())
+#bstr = bson.BSON.encode(str)
 
-myclient = pymongo.MongoClient("mongodb+srv://marcelo:" + urllib.parse.quote("3:s@N<a4ho@K") + "@cluster0-do4dm.azure.mongodb.net/test?retryWrites=true&w=majority")
+with open("/home/marcelo/Imagens/vscode.jpeg","rb") as imagem:
+    bstr = {"imagem" : imagem.read()}
+
+myclient = pymongo.MongoClient("mongodb+srv://chale:ch1234@cluster0-do4dm.azure.mongodb.net/test?retryWrites=true&w=majority")
 mydb = myclient["Chale"]
 mycol = mydb["Camera"]
 
