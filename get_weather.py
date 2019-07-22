a12a87d823d168a297f35b68e136ed73c3b38233
Marcelo <marcelo@lynex.com.br>
2019-07-22 18:11:14 -0300

    first commit

---
+++ get_weather.py
@@ -0,0 +1,37 @@
+import json
+import requests
+import pymongo
+import urllib.parse
+import pymysql
+
+api_token = '8af2fc6096188a91c85c8417aaaa8f74'
+api_url = 'api.openweathermap.org/data/2.5/'
+api_param = 'q=Ibiuna,br'
+
+request = 'https://' + api_url + 'weather?' + api_param + '&appid=' + api_token
+response = requests.get(request)
+
+data = response.json()
+
+main = data['main']
+temp = str(main['temp'] - 273.15)
+humi = str(main['humidity'])
+
+#Insert MySql
+sql = 'INSERT INTO `Weather` (`Date`, `Temperature`, `Humidity`) VALUES (SYSDATE(), ' + temp + ', ' + humi +');'
+conn = pymysql.connect(host='remotemysql.com',
+                       port=3306,
+                       user='1nfP179WJX',
+                       password='DEBCENlorl',
+                       db='1nfP179WJX')
+cursor = conn.cursor()
+#cursor.execute(sql)
+conn.commit()
+conn.close()
+
+#Insert MongoDB
+myclient = pymongo.MongoClient("mongodb+srv://chale:ch1234@cluster0-do4dm.azure.mongodb.net/test?retryWrites=true&w=majority")
+mydb = myclient["Chale"]
+mycol = mydb["Weather"]
+
+x = mycol.insert_one(data)
